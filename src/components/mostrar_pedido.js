5bc2df1791f864cda1a07655c57c1d0dbea64dac
Carlos93099 <saint_3000-death@hotmail.com>
2019-06-18 01:00:03 -0500

    first commit

---
+++ src/components/mostrar_pedido.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import './styles/mostrar_pedido.css'
+
+export default function Mostrar_pedido() {
+    return (
+        <div>
+            <div>
+                <span className="Mostrar_pedido-mp">
+                    Mostrar Pedido
+                </span>
+                <span className="Mostrar_pedido-smn">
+                    SEMANA DEL 20 AL 24 DE MAYO
+                </span>
+                <span className="Mostrar_pedido-cnsms">
+                    Consumos
+                </span>
+                <span className="Mostrar_pedido-ttl">
+                    Total
+                </span>
+                <div className="Mostrar_pedido-ttlnum">
+                    5
+                </div>
+                <div className="Mostrar_pedido-rectangle"></div>
+            </div>
+        </div>
+    )
+}
