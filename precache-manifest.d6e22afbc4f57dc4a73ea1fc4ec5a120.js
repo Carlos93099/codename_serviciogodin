b00c5ba29ee29819abdb214fdcbd0bbcbd840cf0
Carlos93099 <saint_3000-death@hotmail.com>
2019-07-09 21:39:59 -0500

    Updates

---
+++ precache-manifest.d6e22afbc4f57dc4a73ea1fc4ec5a120.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "62b9fad5215cbe59b52bf9cfde0e8d01",
+    "url": "/codename_serviciogodin/index.html"
+  },
+  {
+    "revision": "04ecba4d1c2a161951a3",
+    "url": "/codename_serviciogodin/static/css/main.1a5ba636.chunk.css"
+  },
+  {
+    "revision": "98de27244b3ff7f1d031",
+    "url": "/codename_serviciogodin/static/js/2.904338a0.chunk.js"
+  },
+  {
+    "revision": "04ecba4d1c2a161951a3",
+    "url": "/codename_serviciogodin/static/js/main.8077d365.chunk.js"
+  },
+  {
+    "revision": "7c2f127ef7b0aa74a951",
+    "url": "/codename_serviciogodin/static/js/runtime~main.8049627a.js"
+  },
+  {
+    "revision": "8ca4d8b6f498e7279c70ab0904b6594e",
+    "url": "/codename_serviciogodin/static/media/Fondo.8ca4d8b6.png"
+  },
+  {
+    "revision": "bf086677d240698ba146fa5c907b770c",
+    "url": "/codename_serviciogodin/static/media/Pollo.bf086677.png"
+  },
+  {
+    "revision": "0aa95be44bf88fdf35cb52a816e42e47",
+    "url": "/codename_serviciogodin/static/media/logo_horizontal_azul2.0aa95be4.png"
+  },
+  {
+    "revision": "06d0dbe63f5ecb6608a67ebadff1d401",
+    "url": "/codename_serviciogodin/static/media/mmenublanco.06d0dbe6.png"
+  }
+]);
